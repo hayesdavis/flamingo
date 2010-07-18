04a108c12d6c11cedb38661688bc3fc57f070b97
Hayes Davis <hayes@appozite.com>
2010-07-18 14:34:58 -0500

    Removed config/routes.rb since it's no longer used

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Flamingo::Dispatcher::Map.define do |map|
-  
-  # tweet, event, limit, delete
-  
-  map.all :queue=>:logger #, :worker=>'LogEvents'
-  map.tweet 'text'=>/https?/i, :queue=>:links #, :worker=>'ExtractLinks'
-  map.tweet 'source'=>/web/i, :queue=>:web_source
-
-end
