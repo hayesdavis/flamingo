53d33edeaeb6dc29e3bbe16b713702e16d7f04cb
Hayes Davis <hayes@appozite.com>
2011-02-11 18:54:19 -0800

    First pass at an "adapter" for Gnip. This is basically a stream implementation that uses Gnip's authentication mechanism. Had to do a lot of cookie hacking so there might be a better way. Also, had to hack twitter-stream as well to support cookies.

---
+++ lib/flamingo/adapters/gnip.rb
@@ -0,0 +1,4 @@
+require 'net/http'
+require 'net/https'
+require 'flamingo/adapters/gnip/cookie_jar'
+require 'flamingo/adapters/gnip/stream'
---
+++ lib/flamingo/adapters/gnip/stream.rb
@@ -0,0 +1,52 @@
+module Flamingo
+  module Adapters
+    module Gnip
+      
+      class Stream < Flamingo::Stream
+        
+        attr_accessor :name, :url
+        
+        def initialize(config)
+          self.name = config.name
+          self.url = config.url
+          puts self.url
+        end
+        
+        def connect(options)
+          stream_url, cookies = authenticate(options)
+          stream_connect(stream_url,cookies)
+        end
+        
+        private
+          def stream_connect(stream_url,cookies)
+            uri = URI.parse(stream_url)
+            options = {:host=>uri.host, 
+              :ssl=>(uri.scheme=="https"), 
+              :path=>uri.request_uri,
+              :user_agent=>"Flamingo/#{Flamingo::VERSION}",
+              :auth=>nil,:oauth=>nil,
+              :headers=>{"Cookie"=>cookies.cookie_header_for(uri)}}
+            Twitter::JSONStream.connect(options)
+          end
+          
+          def to_s
+            "[Gnip] #{url}"
+          end
+          
+          def authenticate(options)
+            auth_uri = URI.parse(url)
+            conn = Net::HTTP.new(auth_uri.host,auth_uri.port)
+            conn.use_ssl = true
+            res = conn.start do |http|
+              req = Net::HTTP::Get.new(auth_uri.request_uri)
+              req.basic_auth(options[:username],options[:password])
+              http.request(req)
+            end
+            jar = CookieJar.new(res.get_fields("set-cookie"))
+            [res["location"],jar]
+          end
+        
+      end      
+    end
+  end
+end
