f3567539aba3b09ba394f03cdac9e982c7df9148
Hayes Davis <hayes@appozite.com>
2011-03-04 12:09:18 -0800

    Gnip StreamParams now diff current rules and new rules during adds and only sends new rules to gnip. Changes and errors are logged.

--- lib/flamingo/adapters/gnip/stream_params.rb
+++ lib/flamingo/adapters/gnip/stream_params.rb
@@ -3,7 +3,7 @@ module Flamingo
     module Gnip
       class StreamParams < Flamingo::StreamParams
 
-        attr_accessor :rules
+        attr_accessor :rules, :logger
 
         def initialize(stream_name, rules)
           super(stream_name)
@@ -12,20 +12,24 @@ module Flamingo
 
         def add(key,*values)
           validate_key(key)
-          rules.add(*values)
-          get(key)
+          curr_rules = get(key)
+          # Determine which rules need to be added. Gnip recommends against 
+          # sending all rules every time.
+          new_rules = values - curr_rules
+          unless new_rules.empty?
+            add_rules!(new_rules)
+          end
+          curr_rules + new_rules
         end
 
         def remove(key,*values)
           validate_key(key)
-          rules.delete(*values)
+          delete_rules!(values.flatten)
           get(key)
         end
 
         def delete(key)
-          validate_key(key)
-          rules.delete(*get(key))
-          get(key)
+          remove(key,get(key))
         end
 
         def get(key)
@@ -37,8 +41,41 @@ module Flamingo
         def keys
           [:rules]
         end
+        
+        def logger
+          @logger || Flamingo.logger
+        end
 
         private
+          def add_rules!(new_rules)
+            log_change("add",new_rules)
+            rules.add(*new_rules)
+          rescue => e
+            log_error("add",new_rules,e)
+            raise e
+          end
+          
+          def delete_rules!(to_delete)
+            log_change("delete",to_delete)
+            rules.delete(*to_delete)
+          rescue => e
+            log_error("delete",to_delete,e)
+            raise e
+          end
+          
+          def log_error(type,rules,e)
+            logger.error("Gnip Rules") do
+              "#{type.upcase}: #{rules.join(',')} - "+
+                Logging::Utils.error_trace(e,0)  
+            end
+          end
+          
+          def log_change(type,rules)
+            logger.info("Gnip Rules") do
+              "#{type.upcase}: #{rules.join(',')}"
+            end
+          end
+          
           def validate_key(key)
             unless key.to_sym == :rules
               raise ArgumentError.new("rules is only supported key for this stream")
--- test/adapters/gnip/stream_params_test.rb
+++ test/adapters/gnip/stream_params_test.rb
@@ -5,6 +5,9 @@ class StreamParamsTest < Test::Unit::TestCase
   def setup
     @rules = Flamingo::Adapters::Gnip::Rules.new("http://foo.com/bar","username","password")
     @params = Flamingo::Adapters::Gnip::StreamParams.new("powertrack",@rules) 
+    @params.logger = mock("logger") do
+      stubs(:info=>nil,:error=>nil)
+    end
   end
   
   def test_attempting_to_access_param_besides_rules_fails
@@ -13,21 +16,48 @@ class StreamParamsTest < Test::Unit::TestCase
     end
   end
   
-  def test_add_passes_through_to_rules
+  def test_add_with_no_existng_rules_adds_rules
     @rules.expects(:add).with("a","b","c")
     @rules.expects(:get).returns({:rules=>[]})
-    @params.add(:rules,"a","b","c")
+    all_rules = @params.add(:rules,"a","b","c")
+    assert_equal(%w(a b c),all_rules.sort)
   end
   
-  def test_remove_passes_through_to_rules
+  def test_add_with_existing_rules_adds_to_existing_rules
+    @rules.expects(:add).with("a","b","c")
+    @rules.expects(:get).returns(
+      {:rules=>[{:value=>'x'},{:value=>'y'},{:value=>'z'}]})
+    all_rules = @params.add(:rules,"a","b","c")
+    assert_equal(%w(a b c x y z),all_rules.sort)
+  end
+
+  def test_add_merges_current_rules_and_only_adds_new_rules
+    @rules.expects(:add).with("c","x")
+    @rules.expects(:get).returns(
+      {:rules=>[{:value=>'a'},{:value=>'b'},{:value=>'z'}]})
+    all_rules = @params.add(:rules,"a","b","c","x")
+    assert_equal(%w(a b c x z),all_rules.sort)
+  end
+  
+  def test_add_makes_no_changes_if_rules_already_exist
+    @rules.expects(:add).never
+    @rules.expects(:get).returns(
+      {:rules=>[{:value=>'a'},{:value=>'b'},{:value=>'z'}]})
+    all_rules = @params.add(:rules,"a","b","z")
+    assert_equal(%w(a b z),all_rules.sort)
+  end  
+  
+  def test_remove_results_in_delete_on_rules
     @rules.expects(:delete).with("a","b","c")
     @rules.expects(:get).returns({:rules=>[]})
     @params.remove(:rules,"a","b","c")
   end  
   
-  def test_delete_passes_through_to_rules
+  def test_delete_deletes_all_rules
     state = states('rules').starts_as('init')
-    @rules.expects(:get).returns({:rules=>[{:value=>"a"},{:value=>"b"}]}).when(state.is("init"))
+    @rules.expects(:get).
+      returns({:rules=>[{:value=>"a"},{:value=>"b"}]}).
+      when(state.is("init"))
     @rules.expects(:delete).with("a","b").then(state.is("deleted"))
     @rules.expects(:get).returns({:rules=>[]}).when(state.is("deleted"))
     @params.delete(:rules)
