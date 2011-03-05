d7d5e79adb080ab7e42bdbbe7c067c448381e5f6
Hayes Davis <hayes@appozite.com>
2011-03-04 17:22:47 -0800

    Logging utils can create full or app-only trace for errors.

--- lib/flamingo/logging/utils.rb
+++ lib/flamingo/logging/utils.rb
@@ -7,11 +7,15 @@ module Flamingo
         logger.error error_trace(e,2)
       end
       
-      def error_trace(e,indent=0)
+      def error_trace(e,indent=0,full_trace=false)
         space = " "*indent
         err = "#{space}#{e.class.name}: #{e.message}\n"
         space = " "*(indent+2)
-        err << "#{space}#{e.backtrace.join("\n#{space}")}\n"
+        trace = e.backtrace
+        unless full_trace
+          trace = trace.select{|line| line =~ %r(lib/flamingo)i }
+        end
+        err << "#{space}#{trace.join("\n#{space}")}\n"
         err
       end
       
