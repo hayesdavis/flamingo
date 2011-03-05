325609d1425d9d50d6d4da8146bd8ba153e8df73
Hayes Davis <hayes@appozite.com>
2011-03-04 17:28:01 -0800

    Flamigo REST API server now always runs in production mode.

--- lib/flamingo/daemon/web_server_process.rb
+++ lib/flamingo/daemon/web_server_process.rb
@@ -4,8 +4,8 @@ module Flamingo
       def run
         $0 = 'flamingod-web'
         host, port = Flamingo.config.web.host('0.0.0.0:4711').split(":")
-        Flamingo::Web::Server.run! :host=>host, :port=>port.to_i, 
-          :daemon_pid=>Process.ppid
+        Flamingo::Web::Server.run! :host=>host, :port=>port.to_i,
+          :environment=>:production, :daemon_pid=>Process.ppid
       end
     end
   end  
